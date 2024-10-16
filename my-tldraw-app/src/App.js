01c93defd1cf6c7c1ba78b0fd9b1120b4479445e
Anthony <omega4882@yahoo.com>
2024-10-15 19:43:25 -0600

    updating

---
+++ my-tldraw-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+export default function App() {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+	)
+}
+
+export default App;
